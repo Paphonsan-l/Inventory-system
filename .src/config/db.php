04466f0f70dffd20ea0ca74c768356e1a20f389f
Paphon Leenin <paphonsan.l@ku.th>
2026-02-03 21:50:16 +0700

    config db for deplotment

--- .src/config/db.php
+++ .src/config/db.php
@@ -1,13 +1,26 @@
 <?php
-$host = 'db';
-$dbname = 'inventory_db';
-$username = 'root';
-$password = 'root1234';
+$host = getenv('DB_HOST') ?: 'db';
+$dbname = getenv('DB_NAME') ?: 'inventory_db';
+$username = getenv('DB_USER') ?: 'root';
+$password = getenv('DB_PASSWORD') ?: 'root1234';
+$port = getenv('DB_PORT') ?: 3306;
 
-try{
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}catch(PDOException $e) {
-    die("Connection failed: " . $e->getMessage());
+$options = [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+];
+
+
+if (getenv('DB_HOST')) {
+    $options[PDO::MYSQL_ATTR_SSL_CA] = "/etc/ssl/cert.pem"; // Vercel มีไฟล์นี้ให้อยู่แล้ว
+    $options[PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT] = false;
+}
+
+try {
+    $dsn = "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8mb4";
+    $pdo = new PDO($dsn, $username, $password, $options);
+} catch (PDOException $e) {
+    echo json_encode(['error' => 'Connection failed: ' . $e->getMessage()]);
+    exit;
 }
 ?>
